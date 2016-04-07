fe5d85871f3e44bc02ddf2d2ffbfde68c3913641
donovankryak <thedonovankane@gmail.com>
2016-04-07 13:10:58 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -12,15 +12,21 @@ using namespace std;
 #define ISBLACK(x) (x== BKING || x == BLACK)
 #define ISVACANT(x) (x ==1)
 
-#define ROW 10
-#define COL 10
+#define ROW 8
+#define COL 8
+
+void printDisplay(int board[][COL]);
+void swapIJKL(int board[ROW][COL], int i, int j, int k, int l);
+char pieceprints(int i);
+void printDisplayFancy(int board[][COL]);
+int Playersturn(int board[][COL], int player,int i,int j,int k,int l);
 
 char pieceprints(int i)
 {
 	switch(i)
 	{
 	case 0:
-		return ' ';
+		return '#';
 	case 1: 
 		return ' ';
 	case 2:
@@ -31,8 +37,18 @@ char pieceprints(int i)
 	return ('X');
 }
 
-
 int main(int argc, char *argv[]) {
 	//TODO
+	int board[ROW][COL] = 
+	{
+	{2,1,2,1,2,1,2,1},
+	{1,2,1,2,1,2,1,2},
+	{2,1,2,1,2,1,2,1},
+	{1,0,1,0,1,0,1,0},
+	{0,1,0,1,0,1,0,1},
+	{1,3,1,3,1,3,1,3},
+	{3,1,3,1,3,1,3,1},
+	{1,3,1,3,1,3,1,3},
+	}
 	return 0;
 }
